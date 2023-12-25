f0b021375fa05856dec4562f1ad05814a2261a62
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2023-12-25 19:33:31 +0630

    Install dependencies, auth & root layout structures

---
+++ components/shared/Header.tsx
@@ -0,0 +1,31 @@
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
