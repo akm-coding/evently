758ad8748e3e46565437b9af8a8b9d6d923b88fb
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2023-12-25 20:35:30 +0630

    [Develop & modify] Header, mobile nav & nav items

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,10 @@
-import { SignedOut } from "@clerk/nextjs";
+import { SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -16,7 +18,16 @@ const Header = () => {
             alt="logo"
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">Login</Link>
